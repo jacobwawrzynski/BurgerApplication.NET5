a955540f0c50e76fbdde136183d4d635c2d0102e
Jakub Wawrzyński <kubawawrzynski1@outlook.com>
2022-06-15 01:55:14 +0200

    First commit

--- RandomDataToDataBase/random/RandomStaff.cs
+++ RandomDataToDataBase/random/RandomStaff.cs
@@ -4,56 +4,57 @@ using RandomDataToDataBase.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using RandomDataToDataBase.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RandomDataToDataBase.random
 {
-    public static class RandomStaff
-    {
-        static public Entities.Staff Generate()
-        {
-            var faker = new Faker("pl");
-            var staff = new Entities.Staff();
-            staff.Login = faker.Person.UserName;
-            staff.Password = faker.Random.String(minLength: 8, maxLength: 15, minChar: '9', maxChar: 'z');
-            string[] roles = { "Employee", "Manager", "Owner" };
-            staff.Role = faker.PickRandom(roles);
-            staff.Name = faker.Person.FirstName;
-            staff.Last_Name = faker.Person.LastName;
-            staff.Pesel = faker.Random.Replace("###########").ToString();
-            staff.Email = faker.Person.Email;
-            staff.Creation_Date = DateTime.Now;
-            staff.Deletion_Date = null;
-            Entities.Address address = RandomAddress.Generate();
-            if (DataBase.AddAddressToDataBase(address))
-            {
-                staff.Id_Address = address.Id;
-            }
-            else
-            {
-                throw new Exception("failed to add new address for restaurant");
-            }
-            if (staff.Role == "Owner")
-            {
-                Entities.Restaurant Restaurant = RandomRestaurant.Generate();
-                if (DataBase.AddRestaurantToDataBase(Restaurant))
-                    staff.Id_Restaurant = Restaurant.Id;
+   public static class RandomStaff
+   {
+      static public Entities.Staff Generate()
+      {
+         var faker = new Faker("pl");
+         var staff = new Entities.Staff();
+         staff.Login = faker.Person.UserName;
+         staff.Password = Encryption.ComputeHash(faker.Random.String(minLength: 8, maxLength: 15, minChar: '9', maxChar: 'z'), "SHA512", null);
+         string[] roles = { "Employee", "Manager", "Owner" };
+         staff.Role = faker.PickRandom(roles);
+         staff.Name = faker.Person.FirstName;
+         staff.Last_Name = faker.Person.LastName;
+         staff.Pesel = faker.Random.Replace("###########").ToString();
+         staff.Email = faker.Person.Email;
+         staff.Creation_Date = DateTime.Now;
+         staff.Deletion_Date = null;
+         Entities.Address address = RandomAddress.Generate();
+         if (DataBase.AddAddressToDataBase(address))
+         {
+            staff.Id_Address = address.Id;
+         }
+         else
+         {
+            throw new Exception("failed to add new address for restaurant");
+         }
+         if (staff.Role == "Owner")
+         {
+            Entities.Restaurant Restaurant = RandomRestaurant.Generate();
+            if (DataBase.AddRestaurantToDataBase(Restaurant))
+               staff.Id_Restaurant = Restaurant.Id;
 
-                else
-                    throw new Exception("failed to add new restaurant for owner");
-            }
             else
+               throw new Exception("failed to add new restaurant for owner");
+         }
+         else
+         {
+            using (var db = new AppDbContext())
             {
-                using (var db = new AppDbContext())
-                {
-                    staff.Id_Restaurant = (from r in db.Restaurants
-                                        orderby r.Id
-                                        select r.Id).Last();
-                }
+               staff.Id_Restaurant = (from r in db.Restaurants
+                                      orderby r.Id
+                                      select r.Id).Last();
             }
-            return staff;
-        }
+         }
+         return staff;
+      }
 
-    }
+   }
 }
