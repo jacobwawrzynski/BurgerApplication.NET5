69683f5de3983499e5775e5d565e7ab04a7fceaf
Jakub Wawrzyński <kubawawrzynski1@outlook.com>
2022-06-16 16:44:25 +0200

    Updates sales

--- Dashboard/MVVM/View/SalesView.xaml.cs
+++ Dashboard/MVVM/View/SalesView.xaml.cs
@@ -32,28 +32,9 @@ namespace Dashboard.MVVM.View
          {
             var q = from products
                     in db.Products
-                    select new { products.Name, products.Price, products.Description };
+                    select products;
 
             ProductsDG.ItemsSource = q.ToList();
-
-            //Product_Allergen prd_alg = new Product_Allergen();
-            //Product product = new Product();
-            //Allergen allergen = new Allergen();
-
-            //using (var db = new AppDbContext())
-            //{
-            //   var pr_al = (from pa
-            //                in db.Products_Allergens
-            //                where product.Id == prd_alg.Id_Product
-            //                select pa.Id_Allergen).FirstOrDefault();
-
-            //   var al = from algn
-            //            in db.Allergens
-            //            where allergen.Id == pr_al
-            //            select algn;
-
-            //   AllergensDG.ItemsSource = al.ToList();
-            //}
          }
       }
 
@@ -63,65 +44,42 @@ namespace Dashboard.MVVM.View
          Product product = new Product();
          Allergen allergen = new Allergen();
 
+
          using (var db = new AppDbContext())
          {
-            var pr_al = (from pa
+            var prod = ProductsDG.SelectedItem as Product;
+            int idProd = (from p
+                         in db.Products
+                         where p.Id == prod.Id
+                         select p.Id).FirstOrDefault();
+
+
+            var alerg = (from pa
                          in db.Products_Allergens
-                         where product.Id == prd_alg.Id_Product
-                         select pa.Id_Allergen).FirstOrDefault();
+                         join a in db.Allergens on pa.Id_Allergen equals a.Id
+                         where pa.Id_Product == idProd
+                         select a).ToList();
 
-            var al = from algn
-                     in db.Allergens
-                     where allergen.Id == pr_al
-                     select algn;
+            //var al = (from algn
+            //         in db.Allergens
+            //         where allergen.Id == pr_al
+            //         select algn.Name).FirstOrDefault();
 
-         }
-
-         string str = "";
-         if (ProductsDG.SelectedItems.Count > 0)
-         {
-            foreach (var item in ProductsDG.SelectedItems)
+            string str = "";
+            if (ProductsDG.SelectedItems.Count == 1)
             {
-               allergen = item as Allergen;
-               str += "Allergeny: " + allergen.Name;
-            }
-         }
-         else { }
+               str += $"Allergeny: " ;
+               foreach (var item in alerg)
+               {
+                  str += $"{item.Name} ";
+               }
 
-         AllergensTB.Text = str;
+            }
+
+            AllergensTB.Text = str;
+         }
+
+         
       }
    }
 }
-
-//public MainWindow()
-//{
-//   InitializeComponent();
-//   ObservableCollection<Member> memberData = new ObservableCollection<Member>()
-//             {
-//                 new Member(){DepartmetId = "Dp01",EmployeeId = "EP0011" ,Name = "Joe1",Address = "Street 1",Email = new Uri("mailto:Joe1@school.com")},
-//                 new Member(){DepartmetId = "Dp02",EmployeeId = "EP0012" ,Name = "Joe2",Address = "Street 2",Email = new Uri("mailto:Joe2@school.com")},
-//                 new Member(){DepartmetId = "Dp03",EmployeeId = "EP0013" ,Name = "Joe3",Address = "Street 3",Email = new Uri("mailto:Joe3@school.com")},
-//                 new Member(){DepartmetId = "Dp04",EmployeeId = "EP0014" ,Name = "Joe4",Address = "Street 4",Email = new Uri("mailto:Joe4@school.com")}
-//             };
-//   dataGrid.DataContext = memberData;
-//}
-
-//private void dataGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
-//{
-//   string str = "";
-//   if (dataGrid.SelectedItems.Count > 0)
-//   {
-//      Member member = new Member();
-//      foreach (var obj in dataGrid.SelectedItems)
-//      {
-//         member = obj as Member;
-//         str += "DepartmetId : " + member.DepartmetId + "   EmployeeId:" + member.EmployeeId + "  Name:" + member.Name + "  Address:" + member.Address + "  Email:" + member.Email + "\n";
-//      }
-//   }
-//   else
-//   {
-
-//   }
-//   myTxt.Text = str;
-//}
-//     }
