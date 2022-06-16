32f775f983a1abc04cb3943fd71014fbef2f74e8
Jakub Wawrzyński <kubawawrzynski1@outlook.com>
2022-06-16 03:00:23 +0200

    Employee search

--- Dashboard/MVVM/View/AdminPanelEmpView.xaml.cs
+++ Dashboard/MVVM/View/AdminPanelEmpView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataBaseContext;
+using DataBaseContext.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +26,121 @@ namespace Dashboard.MVVM.View
       {
          InitializeComponent();
       }
+
+      private void SearchBoxEmp_KeyUp(object sender, KeyEventArgs e)
+      {
+         using (var db = new AppDbContext())
+         {
+            int value;
+            if (SearchBoxEmp.Text != "" && int.TryParse(SearchBoxEmp.Text, out value))
+            {
+               var all = from i
+                          in db.Staff
+                         select i.Id;
+
+               if (all.Contains(int.Parse(SearchBoxEmp.Text)))
+               {
+                  var pr = (from employees
+                           in db.Staff
+                            where int.Parse(SearchBoxEmp.Text) == employees.Id
+                            select employees).FirstOrDefault();
+
+                  var adr = (from addresses
+                             in db.Addresses
+                             where pr.Id_Address == addresses.Id
+                             select addresses).First();
+
+                  if (pr != null)
+                  {
+                     LoginField.Text = pr.Login;
+                     PasswordField.Text = pr.Password;
+                     ForenameField.Text = pr.Name;
+                     LastNameField.Text = pr.Last_Name;
+                     PeselField.Text = pr.Pesel;
+                     EmailField.Text = pr.Email;
+                     CreationDateField.Text = pr.Creation_Date.ToString();
+                     RoleField.Text = pr.Role;
+                     CityField.Text = adr.City;
+                     ZipCodeField.Text = adr.Zip_Code;
+                     StreetField.Text = adr.Street;
+                     FlatNumberField.Text = adr.Apartment_Number;
+                     HouseNumberField.Text = adr.House_Number;
+                  }
+
+               }
+            }
+
+         }
+      }
+
+      private void Add_Emp_Btn_Click(object sender, RoutedEventArgs e)
+      {
+         using (var db = new AppDbContext())
+         {
+            Staff employee = new Staff();
+            Address address = new Address();
+
+            address.City = CityField.Text;
+            address.Zip_Code = ZipCodeField.Text;
+            address.Street = StreetField.Text;
+            address.House_Number = HouseNumberField.Text;
+            address.Apartment_Number = FlatNumberField.Text;
+            DataBaseQuery.AddAddressToDataBase(address);
+            db.SaveChanges();
+
+            employee.Login = LoginField.Text;
+            employee.Password = PasswordField.Text;
+            employee.Name = ForenameField.Text;
+            employee.Last_Name = LastNameField.Text;
+            employee.Pesel = PeselField.Text;
+            employee.Role = RoleField.Text;
+            employee.Id_Address = address.Id;
+            DataBaseQuery.AddStaffToDataBase(employee);
+            
+         }
+      }
+
+      private void Remove_Emp_Btn_Click(object sender, RoutedEventArgs e)
+      {
+         using (var db = new AppDbContext())
+         {
+            var pr = from employees
+                     in db.Staff
+                     where employees.Id == int.Parse(SearchBoxEmp.Text)
+                     select employees;
+
+            foreach (var item in pr)
+            {
+               db.Staff.Remove(item);
+            }
+            db.SaveChanges();
+
+         }
+      }
+
+      private void Save_Emp_Btn_Click(object sender, RoutedEventArgs e)
+      {
+         using (var db = new AppDbContext())
+         {
+            var pr = (from employees
+                           in db.Staff
+                      where employees.Id == int.Parse(SearchBoxEmp.Text)
+                      select employees).FirstOrDefault();
+
+            if (pr != null)
+            {
+               pr.Login = LoginField.Text;
+               pr.Password = PasswordField.Text;
+               pr.Pesel = PeselField.Text;
+               pr.Name = ForenameField.Text;
+               pr.Last_Name = LastNameField.Text;
+               pr.Role = RoleField.Text;
+               pr.Email = EmailField.Text;
+               db.SaveChanges();
+            }
+
+         }
+      }
+
    }
 }
